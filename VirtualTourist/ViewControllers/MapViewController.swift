b342b67a7db1aec8f2ebd83966bf24823c693cfb
Yuan <gao308680162@gmail.com>
2022-12-24 20:55:55 -0500

    center of the map can be restored, but the span cannot.

--- VirtualTourist/ViewControllers/MapViewController.swift
+++ VirtualTourist/ViewControllers/MapViewController.swift
@@ -7,63 +7,38 @@
 
 import UIKit
 import MapKit
-extension UserDefaults{
-    func setCenterCoordinate(centerCoordinate:CLLocationCoordinate2D?,forKey key:String){
-        var centerCoordinateData: NSData?
-        if let centerCoordinate = centerCoordinate{
-            do {
-                centerCoordinateData = try NSKeyedArchiver.archivedData(withRootObject: centerCoordinate, requiringSecureCoding: false) as NSData?
-                set(centerCoordinateData,forKey: key)
-                print("centerCoordinateData saved")
-            }
-            catch let erro{
-                print("error archiving CLLocationCoordinate2D data",erro)
-            }
-        }
-    }
-    func setCameraZoomRange(cameraZoomRange:MKMapView.CameraZoomRange?,forKey key:String){
-        var cameraZoomRangeData: NSData?
-        if let cameraZoomRange = cameraZoomRange{
-            do {
-                cameraZoomRangeData = try NSKeyedArchiver.archivedData(withRootObject: cameraZoomRange, requiringSecureCoding: false) as NSData?
-                set(cameraZoomRangeData,forKey: key)
-                print("cameraZoomRangeData saved")
-            }
-            catch _{
-                print("error archiving MKMapView.CameraZoomRange data")
-            }
-        }
-    }
-    func getCameraZoomRange()-> MKMapView.CameraZoomRange?{
-        var cameraZoomRange: MKMapView.CameraZoomRange?
-        if let cameraZoomRangeData = data(forKey: "cameraZoomRange"){
-            do{
-                cameraZoomRange = try NSKeyedUnarchiver.unarchivedObject(ofClass: MKMapView.CameraZoomRange.self, from: cameraZoomRangeData)
-            }
-            catch let error{
-                print("error unarchiving cameraZoomRange data", error)
-            }
-        }
-        return cameraZoomRange
-    }
-}
+
 class MapViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        if let cameraZoomRange = UserDefaults.standard.getCameraZoomRange(){
-            mapView.cameraZoomRange = cameraZoomRange
-            print("cameraZoomRange is assigned in viewDidLoad")
+        if let centerCoordinateDict = UserDefaults.standard.dictionary(forKey: "centerCoordinate"){
+            mapView.region.center.latitude = centerCoordinateDict["lati"] as! CLLocationDegrees
+            mapView.region.center.longitude = centerCoordinateDict["long"] as! CLLocationDegrees
+            print("CenterCoordinate is assigned in viewWillAppear")
+        }
+        if let span = UserDefaults.standard.dictionary(forKey: "span"){
+            mapView.region.span.latitudeDelta = span["latiDelta"] as! CLLocationDegrees
+            mapView.region.span.longitudeDelta = span["longDelta"] as! CLLocationDegrees
         }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        if let cameraZoomRange = UserDefaults.standard.getCameraZoomRange(){
-            mapView.cameraZoomRange = cameraZoomRange
-            print("cameraZoomRange is assigned in viewWillAppear")
+        //if let cameraBoundary = UserDefaults.standard.getCameraBoundary(){
+        //    mapView.cameraBoundary = cameraBoundary
+        //    print("cameraBoundary is assigned in viewWillAppear")
+        //}
+        if let centerCoordinateDict = UserDefaults.standard.dictionary(forKey: "centerCoordinate"){
+            mapView.region.center.latitude = centerCoordinateDict["lati"] as! CLLocationDegrees
+            mapView.region.center.longitude = centerCoordinateDict["long"] as! CLLocationDegrees
+            print("CenterCoordinate is assigned in viewWillAppear")
+        }
+        if let span = UserDefaults.standard.dictionary(forKey: "span"){
+            mapView.region.span.latitudeDelta = span["latiDelta"] as! CLLocationDegrees
+            mapView.region.span.longitudeDelta = span["longDelta"] as! CLLocationDegrees
         }
         
         print("map view appear")
@@ -71,11 +46,13 @@ class MapViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        //var CenterCoordinateDict: [String: Double] = ["lati":mapView.centerCoordinate.latitude,"long":mapView.centerCoordinate.longitude]
-        //UserDefaults.standard.set(CenterCoordinateDict, forKey: "centerCoordinate")
-        //UserDefaults.standard.setCenterCoordinate(centerCoordinate: mapView.centerCoordinate,forKey:"centerCoordinate")
-        UserDefaults.standard.setCameraZoomRange(cameraZoomRange: mapView.cameraZoomRange, forKey: "cameraZoomRange")
-        //UserDefaults.standard.set(mapView.cameraZoomRange,forKey:"zoomRange")
+        var centerCoordinateDict: [String: Double] = ["lati":mapView.region.center.latitude,"long":mapView.region.center.longitude]
+        print("center done")
+        var spanDict: [String: Double] = ["latiDelta":mapView.region.span.latitudeDelta, "longDelta":mapView.region.span.longitudeDelta]
+        print("span done")
+        UserDefaults.standard.set(centerCoordinateDict, forKey: "centerCoordinate")
+        UserDefaults.standard.set(spanDict, forKey: "span")
+
         print("map view disappear")
 
     }
